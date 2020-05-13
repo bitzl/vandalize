cad6ba46c2214ad9e5afa7792debea707b012ef3
Marcus Bitzl <marcus@bitzl.io>
2020-05-13 07:54:55 +0200

    Add spacing option

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,8 @@ fn main() {
             App::new("every")
                 .about("creates a randomized copy for every byte")
                 .arg_from_usage("<input> 'The file to create vandalized copies from.'")
-                .arg_from_usage("<output> 'The directory to put the vandalized copies.'"),
+                .arg_from_usage("<output> 'The directory to put the vandalized copies.'")
+                .arg(Arg::from_usage("-e --every 'Glitch every n-th byte.'").default_value("1")),
         )
         .get_matches();
 
@@ -34,11 +35,17 @@ fn main() {
     let source = Source::new(input);
 
     let target = Path::new(matches.value_of("TARGET").unwrap());
-    let number = usize::from_str_radix(matches.value_of("number").unwrap(), 10).unwrap();
 
     match matches.subcommand_name() {
-        Some("random") => vandalize(input, target, number),
-        Some("every") => every(&source, target),
+        Some("random") => {
+            let number = usize::from_str_radix(matches.value_of("number").unwrap(), 10).unwrap();
+            vandalize(input, target, number)
+        }
+        Some("every") => {
+            let every_nth = usize::from_str_radix(matches.value_of("every").unwrap(), 10).unwrap();
+            // let resolution = usize::from_str_radix(matches.value_of("number").unwrap(), 10).unwrap();
+            every(&source, target, every_nth)
+        }
         Some(other) => println!("Unknown subcommand {}", other),
         None => println!("Must provide subcommand"),
     }
@@ -95,8 +102,8 @@ impl Source {
     }
 }
 
-fn every(source: &Source, target_dir: &Path) {
-    let total = source.len();
+fn every(source: &Source, target_dir: &Path, every_nth: usize) {
+    let total = source.len() / every_nth;
     let digits = total.to_string().len();
 
     let pb = ProgressBar::new(total as u64);
@@ -105,10 +112,11 @@ fn every(source: &Source, target_dir: &Path) {
     ));
 
     for i in 0..total {
+        let byte_index = i * every_nth;
         let filename = format!(
             "{}_v{:0digits$}.{}",
             source.base_name,
-            i,
+            byte_index,
             source.extension,
             digits = digits
         );
@@ -116,7 +124,7 @@ fn every(source: &Source, target_dir: &Path) {
 
         let mut writer: BufWriter<_> = BufWriter::new(File::create(target).unwrap());
 
-        vandalize_bytes(&source.data, &vec![i], &mut writer);
+        vandalize_bytes(&source.data, &vec![byte_index], &mut writer);
         pb.tick();
     }
     pb.finish();
